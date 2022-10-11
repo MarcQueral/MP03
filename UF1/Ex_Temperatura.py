528476d1d5612169e8e22f59a99fa06a1eaeb932
MarcQueral <mqueral@iesebre.com>
2022-10-11 12:36:09 +0200

    canvis

--- UF1/Ex_Temperatura.py
+++ UF1/Ex_Temperatura.py
@@ -3,3 +3,11 @@
 temperaturaC = float (input("Disme la temperatura que fa en graus Celsius: "))
 temperaturaF = ((temperaturaC*1.8)+32)
 print("La temperatura que fa en graus Fahrenheit és:" ,temperaturaF)
+
+'''x = input("Temperatura(ºC): \n");
+try:
+    resultat= float(x)*1,8+32;
+except:
+    print("Has d'introduir un número!!")
+else:
+    print(x,"graus Celsius equivalen a",round(resultat,2),"graus Fahrenheit");'''
