94f9ebcea6c7a82a598ba4e1bfa79aae10ad04f4
MarcQueral <mqueral@iesebre.com>
2022-10-13 14:24:12 +0200

    afegir canvi

--- UF1/diccionaris.py
+++ UF1/diccionaris.py
@@ -1,5 +1,27 @@
-diccionari = {}
+'''diccionari = {}
 diccionari['one'] = 'un'
 diccionari['two'] = 'dos'
 diccionari['three'] = 'tres'
-print(diccionari ['two'], diccionari ['three'])
+print(diccionari ['two'], diccionari ['three'])'''
+
+'''pesos = {}
+pesos['Pepe'] = [30,55,57]
+pesos['Victor'] = [40,64,59]
+pesos['Julia'] = [67,70,65]
+print(pesos['Pepe'])'''
+
+'''pesos = {'Pepe':[30,55,57], 'Victor':[40,64,59], 'Julia':[67,70,65]}
+print(pesos['Pepe'])
+print(len(pesos))
+print('Julia' in pesos)'''
+
+'''pesos = {'Pepe':[30,55,57], 'Victor':[40,64,59], 'Julia':[67,70,65]}
+x=int(input("Introdueix un nou pes per a Pepe: "))
+pesos['Pepe'].append(x)
+print("Pesos de Pepe: ",pesos['Pepe'])'''
+
+pesos = {'Pepe':[30,55,57], 'Victor':[40,64,59], 'Julia':[67,70,65]}
+persona = input("Dis-me a qui vols afegir-li un pes: ")
+x=int(input("Introdueix el nou pes: "))
+pesos[persona].append(x)
+print("Els pesos de", persona, "són:", pesos[persona])
